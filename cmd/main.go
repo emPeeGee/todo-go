a9f7eebc1af380b18c4d5a6e923165bcbb2c3d13
Popovici Mihail <mihail.popovici@happeo.com>
2022-06-02 16:50:41 +0300

    feat: Start connecting database

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/emPeeGee/todo-go/pkg/handler"
 	"github.com/emPeeGee/todo-go/pkg/repository"
 	"github.com/emPeeGee/todo-go/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occurred while running server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,14 @@
+DROP TABLE
+  users;
+
+DROP table
+  todo_lists;
+
+drop table
+  users_lists;
+
+drop table
+  todo_items;
+
+drop table
+  lists_items;
