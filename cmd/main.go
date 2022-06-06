d59d4e2482a98fb4e982babc6bed03f4c8310506
emPeeGee <eempeegee@gmail.com>
2022-06-06 22:19:23 +0300

    feat: Make app graceful

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/emPeeGee/todo-go"
 	"github.com/emPeeGee/todo-go/pkg/handler"
@@ -43,8 +46,27 @@ func main() {
 
 	srv := new(todo.Server)
 
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("Error occurred while running server %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("Error occurred while running server %s", err.Error())
+		}
+	}()
+
+	logrus.Print("Todo App started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	logrus.Print("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("Error occurred on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Error("Error occurred on db connection close: %s", err.Error())
 	}
 }
 
